6981cfe4414e2fdeb32bc13c552dc59be042bd48
Petrov Kirill <ottokirik@gmail.com>
2020-02-06 14:16:21 +0300

    Добавлена библтотека express-rate-limit и миддлвара для лимитирования количества запросов с одного IP

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan'); //Логирование запроса
+const rateLimit = require('express-rate-limit'); //Лимитирование количества запросов с одного IP
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -14,6 +15,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Разрешено 100 запросов в час с одного IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
 app.use(express.json()); // Добавляет объект к req, доступен в req.body
 app.use(express.static(`${__dirname}/public`)); //Статические файлы для которых нужен доступ
 
