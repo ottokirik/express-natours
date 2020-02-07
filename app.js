bc4e00639d893fd2245d0745c4e56ee8022884d2
Petrov Kirill <ottokirik@gmail.com>
2020-02-08 01:41:40 +0300

    Добавлены библиотеки express-mongo-sanitize и xss-clean для очистки входных данных

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan'); //Логирование запроса
 const rateLimit = require('express-rate-limit'); //Лимитирование количества запросов с одного IP
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // Добавляет объект с данными к req, доступен в req.body
 app.use(express.json({ limit: '10kb' })); //Ограничение размера данных отправляемых на сервер
+
+//Очистка входных данных
+app.use(mongoSanitize());
+
+//Отсечка XSS атак
+app.use(xss());
 
 //Статические файлы для которых нужен доступ
 app.use(express.static(`${__dirname}/public`));
