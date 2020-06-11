c0a93e8ae0c4a02957a7bc03d059c5184dd2fcba
Petrov Kirill <ottokirik@gmail.com>
2020-06-11 16:19:03 +0300

    Добавлена возможность cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,11 +27,13 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+//MIDDLEWARES
+app.use(cors());
+app.options('*', cors());
+
 //Статические файлы для которых нужен доступ
 //app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
-
-//MIDDLEWARES
 
 // Установка security HTTP headers
 app.use(helmet());
